48f4c43f35a8a6c28c8b762095df59e506a33c18
inner <10429190+innerNULL@users.noreply.github.com>
2018-11-13 16:36:46 +0800

    Update finetune.py
    add comments

--- finetune.py
+++ finetune.py
@@ -16,29 +16,30 @@ from heapq import nsmallest
 import time
 
 class ModifiedVGG16Model(torch.nn.Module):
-	def __init__(self):
-		super(ModifiedVGG16Model, self).__init__()
+    def __init__(self):
+	super(ModifiedVGG16Model, self).__init__()
 
-		model = models.vgg16(pretrained=True)
-		self.features = model.features
+	model = models.vgg16(pretrained=True)
+	self.features = model.features
 
-		for param in self.features.parameters():
-			param.requires_grad = False
+	for param in self.features.parameters():
+	    param.requires_grad = False
 
-		self.classifier = nn.Sequential(
-		    nn.Dropout(),
-		    nn.Linear(25088, 4096),
-		    nn.ReLU(inplace=True),
-		    nn.Dropout(),
-		    nn.Linear(4096, 4096),
-		    nn.ReLU(inplace=True),
-		    nn.Linear(4096, 2))
+	self.classifier = nn.Sequential(
+            nn.Dropout(),
+	    nn.Linear(25088, 4096),
+	    nn.ReLU(inplace=True),
+	    nn.Dropout(),
+	    nn.Linear(4096, 4096),
+	    nn.ReLU(inplace=True),
+  	    nn.Linear(4096, 2)
+	)
 
-	def forward(self, x):
-		x = self.features(x)
-		x = x.view(x.size(0), -1)
-		x = self.classifier(x)
-		return x
+    def forward(self, x):
+        x = self.features(x)
+	x = x.view(x.size(0), -1)
+	x = self.classifier(x)
+	return x
 
 class FilterPrunner:
 	def __init__(self, model):
